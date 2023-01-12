2c81d26039b5482a975713bcdfe642fe25e5a638
Delta <ti22mau@gmail.com>
2023-01-12 11:17:52 -0300

    Primeiras mudanças

---
+++ POO-CargosLoja/src/RodarAplicativo.java
@@ -0,0 +1,17 @@
+public class RodarAplicativo {
+    public static void main(String[] args) throws Exception {
+        
+        Funcionario funcionario = new Funcionario();
+
+
+        // Upcast
+        Funcionario gerente = new Gerente();
+        Funcionario vendedor = new Vendedor();
+        Funcionario faxineiro = new Faxineiro();
+
+
+
+
+    }
+}
+
